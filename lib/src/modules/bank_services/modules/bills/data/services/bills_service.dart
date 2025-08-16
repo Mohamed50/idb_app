eebeb051bc338ac1a853e16de9257c5dcefbb08c
Mohamed50 <Mohamedalameen50@gmail.com>
2025-08-16 15:34:11 +0400

    Add mockBillInquiry and mockBillPayment methods

--- lib/src/modules/bank_services/modules/bills/data/services/bills_service.dart
+++ lib/src/modules/bank_services/modules/bills/data/services/bills_service.dart
@@ -5,8 +5,8 @@ import 'package:az_banking_app/src/modules/accounts/data/models/account_model.da
 class BillsService extends ApiService {
 
 
-
-  Future<Map<String,dynamic>> billInquiry(String billerId, AccountModel fromAccount, String billNumber, double? amount, String? secondaryNumber) async {
+  Future<Map<String, dynamic>> billInquiry(String billerId, AccountModel fromAccount, String billNumber, double? amount,
+      [String? secondaryNumber]) async {
     final body = {
       'Biller_ID': billerId,
       'Pay_Customer_Code': billNumber,
@@ -18,7 +18,8 @@ class BillsService extends ApiService {
     return response.body;
   }
 
-  Future<Map<String, dynamic>> billPayment(String billerId, AccountModel fromAccount, String billNumber, double amount, String? secondaryNumber) async {
+  Future<Map<String, dynamic>> billPayment(String billerId, AccountModel fromAccount, String billNumber, double amount,
+      [String? secondaryNumber]) async {
     final body = {
       'Biller_ID': billerId,
       'Pay_Customer_Code': billNumber,
@@ -26,7 +27,41 @@ class BillsService extends ApiService {
       'Amount': amount,
       'Account_Info': fromAccount.toJson()
     };
-    final response = await transaction(APIConfiguration.teleBillPaymentUrl, body, onDuplicated:  onDuplicated);
+    final response = await transaction(APIConfiguration.teleBillPaymentUrl, body, onDuplicated: onDuplicated);
     return response.body;
   }
+
+
+  Future<Map<String, dynamic>> mockBillInquiry(String billerId, AccountModel fromAccount, String billNumber, double? amount,
+      [String? secondaryNumber]) async {
+    final body = {
+      'Biller_ID': billerId,
+      'Pay_Customer_Code': billNumber,
+      'Additional_Reference': secondaryNumber ?? '',
+      'Amount': amount ?? '',
+      'Account_Info': fromAccount.toJson(),
+      'Pay_Flag': 0,
+    };
+    final response = await post(APIConfiguration.mockBillPaymentUrl, body);
+    return response.body;
+  }
+
+  Future<Map<String, dynamic>> mockBillPayment(String billerId, AccountModel fromAccount, String billNumber, double amount,
+      [String? secondaryNumber]) async {
+    final body = {
+      'Biller_ID': billerId,
+      'Pay_Customer_Code': billNumber,
+      'Amount': amount,
+      'Account_Info': fromAccount.toJson(),
+      'Pay_Flag': 1,
+    };
+    if (secondaryNumber != null) {
+      body['Additional_Reference'] = secondaryNumber;
+    }
+    final response = await transaction(APIConfiguration.mockBillPaymentUrl, body, onDuplicated: onDuplicated);
+    return response.
+    body;
+  }
+
+
 }
